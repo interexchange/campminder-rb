1c381af44a7f95e42854388ee4b23e636010ff2e
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-01-30 12:34:38 -0500

    hound

--- lib/CampMinder.rb
+++ lib/CampMinder.rb
@@ -5,4 +5,4 @@ require "active_model_serializers"
 module CampMinder
 end
 
-Dir[File.dirname(__FILE__) + "/CampMinder/**/*.rb"].each {|file| require file }
+Dir[File.dirname(__FILE__) + "/CampMinder/**/*.rb"].each { |file| require file }
