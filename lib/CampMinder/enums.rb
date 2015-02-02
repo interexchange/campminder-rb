736ceb5c5b45cc1f9fd3faaa2c3a0a27f95e7e3a
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-02-02 16:00:19 -0500

    enums

---
+++ lib/CampMinder/enums.rb
@@ -0,0 +1,15 @@
+module CampMinder::Enums
+  module MaritalStatus
+    UNDEFINED = 0
+    MARRIED = 1
+    DIVORCED = 2
+    SEPARATED = 3
+    SINGLE = 4
+    WIDOWED = 5
+  end
+
+  module Gender
+    FEMALE = 0
+    MALE = 1
+  end
+end
