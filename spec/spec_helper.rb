7cdcaecf3c170a5c02b48f986d23dfe3c1eb50d6
Aaron Todd <ozzyaaron@gmail.com>
2017-04-17 11:44:46 -0600

    Update ruby & Gems, remove code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,6 @@ ENV["RACK_ENV"] ||= "test"
 require "dotenv"
 Dotenv.load
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require "pry"
 require "rack/test"
 require "timecop"
@@ -22,12 +19,9 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
 
-WebMock.disable_net_connect!(allow: "codeclimate.com")
-
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.ignore_localhost = true
-  config.ignore_hosts "codeclimate.com"
   config.hook_into :webmock
 end
 
