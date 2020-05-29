7122745a60fa8d77651fcb3036b216cf74ecf2a4
Aaron Todd <ozzyaaron@gmail.com>
2020-05-29 12:01:53 -0600

    Update for Rails 6

--- lib/CampMinder/controllers/staff_hire_controller.rb
+++ lib/CampMinder/controllers/staff_hire_controller.rb
@@ -1,6 +1,6 @@
 module CampMinder::StaffHireController
   def create
-    @camp_minder_staff_hire = CampMinder::StaffHire.new(params)
+    @camp_minder_staff_hire = CampMinder::StaffHire.new(params.permit!)
 
     if @camp_minder_staff_hire.post && store_staff_hire
       render status: 200, json: success_response
